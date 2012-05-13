c279e530e8848f0ed156bc88092144df04d9f3a7
heartyoh <heartyoh@gmail.com>
2012-05-13 13:30:11 +0900

    report nav.

--- app/view/Nav/NavReport.js
+++ app/view/Nav/NavReport.js
@@ -19,25 +19,31 @@ Ext.define('HatioBB.view.nav.NavReport', {
 			fields : ['reportId', 'desc'],
 			data : [{
 				reportId : 'chart_v_health',
+				name : 'Vehicle Health',
 				desc : 'Vehicle Health'
 			}, {
 				reportId : 'report1',
+				name : 'Line Chart',
 				desc : 'Report Type 1'
 			}, {
 				reportId : 'report2',
+				name : 'Bar Chart I',
 				desc : 'Report Type 2'
 			}, {
 				reportId : 'report3',
+				name : 'Bar Chart II',
 				desc : 'Report Type 3'
 			}, {
 				reportId : 'report4',
+				name : 'Area Chart',
 				desc : 'Report Type 4'
 			}, {
 				reportId : 'report5',
+				name : 'Pie Chart',
 				desc : 'Report Type 5'
 			}]
 		}),
 
-        itemTpl: '<div class="iconChart"><strong>{reportId}</strong> {desc}</div>'
+        itemTpl: '<div class="iconChart"><strong>{name}</strong> {desc}</div>'
     }
 });
