1f99c3f1fc82ba2b22e98009e86c8a55ae490fb4
heartyoh <heartyoh@gmail.com>
2012-05-23 22:25:02 +0900

    vehicle, driver nav..

--- app/controller/Main.js
+++ app/controller/Main.js
@@ -1,7 +1,7 @@
 Ext.define('HatioBB.controller.Main', {
     extend: 'Ext.app.Controller',
 
-	requires : ['HatioBB.store.SubMenus', 'HatioBB.view.Setting', 'HatioBB.view.Search', 'HatioBB.view.driver.Driver', 'HatioBB.view.vehicle.Vehicle'],
+	requires : ['HatioBB.store.SubMenus', 'HatioBB.view.Setting', 'HatioBB.view.driver.Driver', 'HatioBB.view.vehicle.Vehicle'],
 	
     config: {
 		routes : {
